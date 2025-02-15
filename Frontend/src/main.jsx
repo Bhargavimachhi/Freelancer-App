861b6260f54ca3d730622c0585dea36a3888a5b8
Bhargavimachhi <140937880+Bhargavimachhi@users.noreply.github.com>
2025-02-15 15:14:38 +0530

    Completed authentication of user

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -2,9 +2,18 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { ClerkProvider } from '@clerk/clerk-react'
+
+const PUBLISHABLE_KEY = "pk_test_a2V5LWJvbmVmaXNoLTM2LmNsZXJrLmFjY291bnRzLmRldiQ"
+
+if (!PUBLISHABLE_KEY) {
+  throw new Error('Add your Clerk Publishable Key to the .env.local file')
+}
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl="/">
+      <App />
+    </ClerkProvider>
   </StrictMode>,
 )
