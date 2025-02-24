feb936cd7032a911a81b16346dd1e9cad13d56be
Bhargavimachhi <140937880+Bhargavimachhi@users.noreply.github.com>
2025-02-24 18:00:11 +0530

    Changed Loading page ui

--- Frontend/src/components/LoadingPage.jsx
+++ Frontend/src/components/LoadingPage.jsx
@@ -1,18 +1,15 @@
-import React from "react";
-import Error from "../assets/images/loading.webp";
+import { motion } from "framer-motion";
 
-const LoadinPage = () => {
+const LoadingPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen px-6 text-center bg-white">
-      {/* UFO Image */}
-      <img src={Error} alt="UFO beaming cat" className="w-64 md:w-80" />
-
-      {/* Title */}
-      <h1 className="mt-6 text-2xl font-semibold text-gray-900 md:text-3xl">
-        Output is waiting for you too
-      </h1>
+    <div className="flex items-center justify-center h-screen">
+      <motion.div
+        className="w-16 h-16 border-4 border-blue-500 border-t-transparent rounded-full animate-spin"
+        animate={{ rotate: 360 }}
+        transition={{ repeat: Infinity, duration: 1, ease: "linear" }}
+      />
     </div>
   );
 };
 
-export default LoadinPage;
+export default LoadingPage;
